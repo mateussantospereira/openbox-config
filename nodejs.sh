fa6c1f0d97ea858dea5067a399f972b06afdc615
mateussantospereira <matpereira0111@gmail.com>
2025-04-19 16:05:34 -0300

    Node JS

--- nodejs.sh
+++ nodejs.sh
@@ -1,2 +1,2 @@
-curl -fsSL https://deb.nodesource.com/setup_22.x | su -c bash -
-su -c apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+apt-get install -y nodejs
